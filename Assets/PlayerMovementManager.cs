610553676f85803625cc6b8849dc59e4a3a91e8b
Oguzhan Gultekin <oguzhangultekin098@gmail.com>
2021-03-10 22:18:17 +0300

    VV
    VV

--- Assets/PlayerMovementManager.cs
+++ Assets/PlayerMovementManager.cs
@@ -2,8 +2,7 @@
 
 public class PlayerMovementManager : MonoBehaviour
 {
-    float power;
-    public float jumpPower = 5f;
+    public float jumpPower;
     Vector2 fingerPoint;
     Vector2 jumpVector;
     float xAngle;
@@ -16,7 +15,6 @@ public class PlayerMovementManager : MonoBehaviour
         _PlayerCollisionManager = GetComponent<CollisionManager>();
         _PlayerRigidBody = GetComponent<Rigidbody2D>();
         _InputManager = GetComponent<InputManager>();
-        power = 5f;
     }
 
 
@@ -36,81 +34,17 @@ public class PlayerMovementManager : MonoBehaviour
             {
                 Vector2 jumpDirectionVector = calculateJumpDirectionVector(_InputManager.TouchEndPos2D, _InputManager.touchHoldTime);
                 Debug.Log(jumpDirectionVector);
-                _PlayerRigidBody.AddForce(jumpDirectionVector * jumpPower*25f);
+                _PlayerRigidBody.AddForce(jumpDirectionVector * jumpPower);
 
             }
-            /*
-                        if (Input.GetMouseButtonDown(0))
-                        {
-
-                            fingerPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                            jumpVector = (fingerPoint - (Vector2)transform.position).normalized;
-
-                            xAngle = Vector2.Angle(jumpVector, new Vector2(jumpVector.x, 0));
-                            if (fingerPoint.x < transform.position.x)
-                            {
-                                gameObject.transform.localScale = new Vector3(-Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);
-                                if (xAngle < 30)
-                                {
-                                    jumpVector = new Vector2(-0.8f, 0.6f);
-                                }
-                                else if (xAngle > 75)
-                                {
-                                    jumpVector = new Vector2(-0.25f, 1.0f);
-                                }
-                            }
-                            else if (fingerPoint.x > transform.position.x)
-                            {
-                                gameObject.transform.localScale = new Vector3(Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);
-                                if (xAngle < 30)
-                                {
-                                    jumpVector = new Vector2(0.8f, 0.6f);
-                                }
-                                else if (xAngle > 75)
-                                {
-                                    jumpVector = new Vector2(0.25f, 1.0f);
-                                }
-                            }
-
-                        }
-                        else if (Input.GetMouseButton(0))
-                        {
-                            power += Time.deltaTime;
-                            if (power > 1.5f)
-                            {
-                                power = 1.5f;
-                            }
-                        }
-                        else if (Input.GetMouseButtonUp(0))
-                        {
-
-                            _PlayerRigidBody.AddForce(jumpVector * power * jumpPower);
-                            power = 0;
-                        }
-                    }
-                    else
-                    {
-                        if (_PlayerRigidBody.velocity.y == 0)
-                        {
-                            if (_PlayerRigidBody.velocity.magnitude > 0.5f)
-                            {
-                                _PlayerRigidBody.velocity = _PlayerRigidBody.velocity.normalized * 0.5f;
-                            }
-                        }
-                        else if (_PlayerRigidBody.velocity.y < 0)
-                        {
-                            _PlayerRigidBody.velocity = _PlayerRigidBody.velocity * 1.01f;
-
-                        }
-            */
         }
 
     }
 
     private Vector2 calculateJumpDirectionVector(Vector2 touchEndPosition, float touchHoldTime)
     {
-        if (touchHoldTime>2f)//min ve max zıplama bunun ile ayarlanacak
-            touchHoldTime = 2f;
+        if (touchHoldTime>1.5f)//min ve max zıplama bunun ile ayarlanacak
+            touchHoldTime = 1.5f;
         else if (touchHoldTime<0.3f)
             touchHoldTime = 0.3f;
 
@@ -145,8 +79,8 @@ public class PlayerMovementManager : MonoBehaviour
             }
         }
 
-
-        return jumpVector;
+        Debug.Log("Here");
+        return jumpVector*touchHoldTime;
     }
 
     void setBoundariesforPlayerPosition()
